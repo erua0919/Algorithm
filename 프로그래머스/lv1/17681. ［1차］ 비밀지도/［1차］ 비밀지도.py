ded8ffacb742db7f9a923d8d8a505c24e0cbd996
erua0919 <58067898+erua0919@users.noreply.github.com>
2022-09-08 22:59:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -3,8 +3,3 @@ def solution(n, arr1, arr2):
     for i in range(n):
         answer.append(bin(arr1[i]|arr2[i])[2:].zfill(n).replace('0',' ').replace('1', '#'))
     return answer
-n = 5
-arr1 = [9,20,28,18,11]
-arr2 = [30,1,21,17,28]
-
-print(solution(n,arr1,arr2))
