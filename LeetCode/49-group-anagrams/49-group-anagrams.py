e9b5347b0a6720f9dad57bddf260dfa0fcade236
DaHam Kim <58067898+erua0919@users.noreply.github.com>
2022-10-28 22:40:02 +0900

    Time: 218 ms (50.71%), Space: 17.1 MB (96.53%) - LeetHub

---
+++ LeetCode/49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,6 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = collections.defaultdict(list)
+        for str in strs:
+            result[''.join(sorted(str))].append(str)
+        return list(result.values())
