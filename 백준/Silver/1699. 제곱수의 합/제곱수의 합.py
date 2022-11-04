aed8ba8d180883a9c61cd0cc1f103c2ba41bcbcf
DaHam Kim <58067898+erua0919@users.noreply.github.com>
2022-11-04 23:00:03 +0900

    [Silver II] Title: 제곱수의 합, Time: 188 ms, Memory: 115764 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [0]*(n+1)
+for i in range(1,n+1):
+    dp[i] = i
+    for j in range(1,i):
+        if j**2 > i:
+            break
+        dp[i] = min(dp[i], dp[i-j**2]+1)
+print(dp[n])
